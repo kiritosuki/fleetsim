a62aea307f28c913df38bd6d03b72dce03d6abe4
kirito <3322640054@qq.com>
2025-11-14 23:50:20 +0800

    删除env.dev文件 猪脑过载了 想了一下这个文件不应该推送 大家需要自己写一个.env.dev文件放自己的环境配置 这里有开发一半的代码 尽量不要回退到这个commit上

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,6 +13,12 @@ var DB *gorm.DB
 
 // InitDB 初始化数据库
 func InitDB() {
+	// 尝试读取配置文件 放入到环境变量中
+	envErr := godotenv.Load(".env.dev")
+	if envErr != nil {
+		// 报错表示找不到.env.dev文件
+	}
+
 	// dsn DataSourceName 数据库连接字符串
 	// 格式：<username>:<password>@tcp(<ip>:<port>)/<数据库名>?<参数设置>=<...>
 	dsn := "root:1234@tcp(127.0.0.1:3306)/routing?charset=utf8mb4&parseTime=True&loc=Local"
