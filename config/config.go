930cddcec403be6903fc25bbacc2d17aa0c80eb0
kirito <3322640054@qq.com>
2025-11-15 12:30:33 +0800

    避免把配置信息硬编码在代码中 该用配置文件+环境变量的方式 保证开发环境和线上环境可以共用一套代码 开发环境使用时需要在项目根目录下添加.env.dev文件 指定数据库用户 密码 ip 端口 database 线上环境需要在部署流程中把相关信息写入环境变量

--- config/config.go
+++ config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,14 +15,23 @@ var DB *gorm.DB
 // InitDB 初始化数据库
 func InitDB() {
 	// 尝试读取配置文件 放入到环境变量中
-	//envErr := godotenv.Load("")
-	//if envErr != nil {
-	// 报错表示找不到.env.dev文件
-	//}
+	envErr := godotenv.Load(".env.dev")
+	if envErr != nil {
+		// 报错表示找不到.env.dev文件 则使用默认的环境变量配置（线上环境变量配置在了devops中）
+		fmt.Println("[Warning] .env.dev not found，采用默认环境变量")
+	}
+
+	// 获取环境变量
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
 	// dsn DataSourceName 数据库连接字符串
 	// 格式：<username>:<password>@tcp(<ip>:<port>)/<数据库名>?<参数设置>=<...>
-	dsn := "root:1234@tcp(127.0.0.1:3306)/routing?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:1234@tcp(127.0.0.1:3306)/routing?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 	// 创建数据库连接对象
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
